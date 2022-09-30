bb913280715674be4688035a7fa41366618db7c6
daiyafei <simon.dyf@qq.com>
2022-09-30 15:31:27 +0800

    none

--- deploy.sh
+++ deploy.sh
@@ -11,3 +11,4 @@ hexo g
 
 hexo d
 
+curl --location --request POST --X POST 'https://cn-n1-console-api.leancloud.cn/1.1/engine/groups/web/production/version?gitTag=master&token=Fu0NLSaciBgBWDzhh0ek6nPOVkQdMHZGIqbhB9hyzERatmZrSB799NEFUHQck1vD'
